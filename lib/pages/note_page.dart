8b1bc727add9a8c9ca7c396bf2fcf6217f5b4dfa
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-11-04 20:16:01 +0530

    update

--- lib/pages/note_page.dart
+++ lib/pages/note_page.dart
@@ -20,7 +20,7 @@ class NotePage extends StatelessWidget {
           children: [
             const SizedBox(height: 20),
             const Text(
-              "KOHOMADA YALUWANEee",
+              "KOHOMADA YALUWANEeee",
               style: AppTextStyles.appTitle,
             ),
             const SizedBox(height: 5),
