2f747f1321d4594f4ef33c1ad84e28bb8e7e1eee
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-11-06 23:45:54 +0530

    pending

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -83,7 +83,7 @@ class _HomePageState extends State<HomePage> {
                       height: MediaQuery.of(context).size.height * 0.5,
                       child: const Center(
                         child: Text(
-                          "No notes available, click on the + button to add a new noteeeeee",
+                          "No notes available, click on the + button to add a new noteeeeeee",
                           style: AppTextStyles.descriptionLargeStyle,
                           textAlign: TextAlign.center,
                         ),
