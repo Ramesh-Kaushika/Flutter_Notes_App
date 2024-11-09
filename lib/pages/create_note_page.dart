475695d9b3299ece4ec1d5125ac4ec7acf528546
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-11-09 23:50:59 +0530

    update

--- lib/pages/create_note_page.dart
+++ lib/pages/create_note_page.dart
@@ -76,7 +76,7 @@ class _CreateNotePageState extends State<CreateNotePage> {
                       controller: titleController,
                       validator: (value) {
                         if (value == null || value.isEmpty) {
-                          return 'Please enter a titlee23456';
+                          return 'Please enter a titlee234567';
                         }
                         return null;
                       },
