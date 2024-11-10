9cdeee1a1412713fdb4a206503c1beccbd428eb9
Ramesh Kaushika De Silva <ramesh.kaushika2245@gmail.com>
2024-11-10 19:04:25 +0530

    new feature add

--- lib/pages/create_note_page.dart
+++ lib/pages/create_note_page.dart
@@ -7,7 +7,7 @@ import 'package:notes_app/utils/router.dart';
 import 'package:notes_app/utils/text_styles.dart';
 
 class CreateNotePage extends StatefulWidget {
-  final Item? item; // Optional item parameter for editing 2345668910
+  final Item? item; // Optional item parameter for editing 234566891011
 
   const CreateNotePage({super.key, this.item});
 
